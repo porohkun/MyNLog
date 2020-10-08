7df3eb5665547981571ac29aac3ac6d169667c65
poroh-kun <meta.poroh.kun@gmail.com>
2020-10-08 16:18:17 +0300

    project init

---
+++ MyNLog/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MyNLog.Services;
+using MyNLog.Views;
+using Prism.Ioc;
+using Prism.Unity;
+using System.Diagnostics;
+using System.Windows;
+
+namespace MyNLog
+{
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override Window CreateShell()
+        {
+            var w = Container.Resolve<MainWindow>();
+            return w;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            PresentationTraceSources.DataBindingSource.Listeners.Add(Container.Resolve<BindingErrorTraceListener>());
+            PresentationTraceSources.DataBindingSource.Switch.Level = SourceLevels.Error;
+        }
+    }
+}
---
+++ MyNLog/Services/BindingErrorTraceListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace MyNLog.Services
+{
+    public class BindingErrorTraceListener : TraceListener
+    {
+        protected static readonly NLog.Logger Logger = NLog.LogManager.GetLogger(typeof(BindingErrorTraceListener).ToString());
+
+        private readonly StringBuilder _messageBuilder = new StringBuilder();
+
+        public override void Write(string message)
+        {
+            _messageBuilder.Append(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Write(message);
+
+            var fullMessage = _messageBuilder.ToString();
+            Logger.Warn(fullMessage);
+            _messageBuilder.Clear();
+        }
+    }
+}
