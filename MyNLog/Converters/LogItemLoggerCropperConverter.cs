a10574923bd85229e034c9e3089c42c59af730bf
alexandersevbo <47869698+alexandersevbo@users.noreply.github.com>
2023-03-19 18:24:33 +0300

    Update LogItemLoggerCropperConverter.cs
    correcting one-direction ConvertBack converter method

--- MyNLog/Converters/LogItemLoggerCropperConverter.cs
+++ MyNLog/Converters/LogItemLoggerCropperConverter.cs
@@ -16,7 +16,7 @@ namespace MyNLog.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return Binding.DoNothing;
         }
     }
 }
